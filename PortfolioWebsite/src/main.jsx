71fb987ba107f95e14239a5a120dd50fe94506bd
Redemption882 <loquendohyper@gmail.com>
2025-10-02 00:56:09 +0200

    Removed duplicated files from master branch

--- PortfolioWebsite/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
-import Portfolio from './components/portfolioMain.jsx'
-import "./main.css"
-
-createRoot(document.getElementById('root')).render(<Portfolio/>)
